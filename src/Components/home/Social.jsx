1367c0354b43d65f5d4b9ff794bb002a0395c117
juhithaV <juhithavemuri@gmail.com>
2024-12-01 16:42:55 +0530

    Home Page

--- src/Components/home/Social.jsx
+++ src/Components/home/Social.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        a.
+        <a href="www.instagram.com" className="home__social-icon" target="_blank">
+          <i className="uil uil-instagram"></i>
+        </a>
+        <a href="www.youtube.com/@juhithaVemuri" className="home__social-icon" target="_blank">
+        <i className="uil uil-youtube"></i>
+        </a>
+        <a href="www.github.com/juhithaV" className="home__social-icon" target="_blank">
+        <i className="uil uil-github-alt"></i>
+        </a>
+
     </div>
   )
 }
